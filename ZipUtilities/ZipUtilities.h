ebc038964db5719ed44f61c6af1ded028a64443d
Ashton Williams <AJWilliams@odecee.com.au>
2015-09-05 14:40:20 +1000

    Removed needless imports from umbrella header

--- ZipUtilities/ZipUtilities.h
+++ ZipUtilities/ZipUtilities.h
@@ -25,11 +25,7 @@
 //  SOFTWARE.
 //
 
-#if TARGET_OS_IPHONE
-#import <UIKit/UIKit.h>
-#else
-#import <Cocoa/Cocoa.h>
-#endif
+#import <Foundation/Foundation.h>
 
 //! Project version number for ZipUtilities.
 FOUNDATION_EXPORT double ZipUtilitiesVersionNumber;
