722fa06bde15525cab533f815c8255b93c1cf76f
Nolan O'Brien <nsprogrammer@nsprogrammer.com>
2015-12-15 10:13:25 -0800

    Fix decompress failing with Z_BUF_ERROR
    Z_BUF_ERROR is not fatal, just needs to be handled
    by providing more input or expanding the output buffer.
    
    Add unit tests.  Happens most often with large files
    being decompressed.

--- ZipUtilitiesTests/NOZSwiftTests.swift
+++ ZipUtilitiesTests/NOZSwiftTests.swift
@@ -41,6 +41,16 @@ func TearDownZipQueue()
 @objc class NOZSwiftCompressionTests: XCTestCase, NOZCompressDelegate
 {
 
+    class func prepareFileForCompression(fileName: String) throws -> String
+    {
+        let tmpDir : NSString = NSTemporaryDirectory()
+        let filePath = tmpDir.stringByAppendingPathComponent(fileName)
+        let bundle = NSBundle.init(forClass: self)
+        let sourceFilePath = bundle.pathForResource(fileName, ofType: nil)!
+        try NSFileManager.defaultManager().copyItemAtPath(sourceFilePath, toPath: filePath)
+        return filePath
+    }
+
     override class func setUp()
     {
         SetUpZipQueue()
@@ -64,9 +74,10 @@ func TearDownZipQueue()
         let op = startCompression()
         op.waitUntilFinished()
         XCTAssertTrue(op.finished)
+        XCTAssertTrue(op.result.didSucceed)
     }
 
-    func startCompression() -> NSOperation
+    func startCompression() -> NOZCompressOperation
     {
         let tmpDir : NSString = NSTemporaryDirectory()
         let zipFilePath = tmpDir.stringByAppendingPathComponent("Mixed.zip")
@@ -100,10 +111,51 @@ func TearDownZipQueue()
         })
     }
 
+    func testLargeZipCompress() {
+        var result:NOZCompressResult? = nil
+        let filePath = try! NOZSwiftCompressionTests.prepareFileForCompression("Star.Wars.7.Trailer.mp4") as NSString
+        let expectation = expectationWithDescription("completion closure is called");
+        let request = NOZCompressRequest(destinationPath: filePath.stringByAppendingPathExtension("zip")! as String)
+        request.addFileEntry(filePath as String)
+        let operation = NOZCompressOperation(request: request, completion: {
+            (op, res) -> Void in
+            result = res
+            expectation.fulfill()
+        })
+
+        zipQueue?.addOperation(operation)
+        waitForExpectationsWithTimeout(5.0 * 60.0, handler: nil)
+
+        if let realResult = result
+        {
+            XCTAssert(realResult.didSucceed)
+        }
+    }
+
+    func testLargeZipCompressWithAppleCoder() {
+        if #available(iOS 9.0, *) {
+            let oldEncoder = NOZEncoderForCompressionMethod(NOZCompressionMethod.Deflate)
+            NOZUpdateCompressionMethodEncoder(NOZCompressionMethod.Deflate, NOZXAppleCompressionCoder.encoderWithAlgorithm(COMPRESSION_ZLIB))
+            testLargeZipCompress()
+            NOZUpdateCompressionMethodEncoder(NOZCompressionMethod.Deflate, oldEncoder)
+        }
+    }
+
 }
 
 @objc class NOZSwiftDecompressionTests: XCTestCase, NOZDecompressDelegate
 {
+
+    class func prepareZipFileForDecompression(fileName: String) throws -> String
+    {
+        let tmpDir : NSString = NSTemporaryDirectory()
+        let zipFilePath = tmpDir.stringByAppendingPathComponent(fileName + ".zip")
+        let bundle = NSBundle.init(forClass: self)
+        let sourceFilePath = bundle.pathForResource(fileName, ofType: "zip")!
+        try NSFileManager.defaultManager().copyItemAtPath(sourceFilePath, toPath: zipFilePath)
+        return zipFilePath
+    }
+
     override class func setUp()
     {
         SetUpZipQueue()
@@ -127,16 +179,12 @@ func TearDownZipQueue()
         let op = startDecompression()
         op.waitUntilFinished()
         XCTAssertTrue(op.finished)
+        XCTAssertTrue(op.result.didSucceed)
     }
 
-    func startDecompression() -> NSOperation
+    func startDecompression() -> NOZDecompressOperation
     {
-        let tmpDir : NSString = NSTemporaryDirectory()
-        let zipFilePath = tmpDir.stringByAppendingPathComponent("Mixed.zip")
-        let bundle = NSBundle.init(forClass: self.dynamicType)
-        let sourceFilePath = bundle.pathForResource("Mixed", ofType: "zip")!
-        try! NSFileManager.defaultManager().copyItemAtPath(sourceFilePath, toPath: zipFilePath)
-
+        let zipFilePath = try! NOZSwiftDecompressionTests.prepareZipFileForDecompression("Mixed")
         let request = NOZDecompressRequest.init(sourceFilePath: zipFilePath)
         let operation = NOZDecompressOperation.init(request: request, delegate: self)
         zipQueue?.addOperation(operation)
@@ -157,14 +205,34 @@ func TearDownZipQueue()
             // NSLog("Progress: \(progress)%")
         })
     }
-    
-    func testCompletionClosureIsCalled() {
-        let expectation = expectationWithDescription("completion closure is called")
-        let request = NOZDecompressRequest(sourceFilePath: "")
-        let operation = NOZDecompressOperation(request: request) { (operation, result) -> Void in
+
+    func testLargeZipDecompress() {
+        var result:NOZDecompressResult? = nil
+        let zipFilePath = try! NOZSwiftDecompressionTests.prepareZipFileForDecompression("Star.Wars.7.Trailer.mp4")
+        let expectation = expectationWithDescription("completion closure is called");
+        let request = NOZDecompressRequest(sourceFilePath: zipFilePath)
+        let operation = NOZDecompressOperation(request: request, completion: {
+            (op, res) -> Void in
+            result = res
             expectation.fulfill()
-        }
+        })
+
         zipQueue?.addOperation(operation)
-        waitForExpectationsWithTimeout(10.0, handler: nil)
+        waitForExpectationsWithTimeout(5.0 * 60.0, handler: nil)
+
+        if let realResult = result
+        {
+            XCTAssert(realResult.didSucceed)
+        }
     }
+
+    func testLargeZipDecompressWithAppleCoder() {
+        if #available(iOS 9.0, *) {
+            let oldDecoder = NOZDecoderForCompressionMethod(NOZCompressionMethod.Deflate)
+            NOZUpdateCompressionMethodDecoder(NOZCompressionMethod.Deflate, NOZXAppleCompressionCoder.decoderWithAlgorithm(COMPRESSION_ZLIB))
+            testLargeZipDecompress()
+            NOZUpdateCompressionMethodDecoder(NOZCompressionMethod.Deflate, oldDecoder)
+        }
+    }
+
 }
